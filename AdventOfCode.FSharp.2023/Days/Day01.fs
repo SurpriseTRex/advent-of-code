e10aebb72f1c0189ec6ec7ebbf6cfe1e1b8f8f89
Sean Johnston <sean.c.johnston@gmail.com>
2023-12-04 10:23:59 +0000

    remove unnecessary type annotations

--- AdventOfCode.FSharp.2023/Days/Day01.fs
+++ AdventOfCode.FSharp.2023/Days/Day01.fs
@@ -6,13 +6,13 @@ open System.IO
 let readExample = File.ReadLines("Inputs/01/example_01.txt")
 let readInput = File.ReadLines("Inputs/01/input_01.txt")
 
-let toChars (input: string) = List.ofSeq input
+let toChars input = List.ofSeq input
 
-let getDigits (input: char list) = input |> List.filter Char.IsDigit
+let getDigits input = input |> List.filter Char.IsDigit
 
-let getFirstAndLast (input: char list) = (List.head input, List.last input)
+let getFirstAndLast input = (List.head input, List.last input)
 
-let convertToInt (a: char, b: char) = $"{a}{b}" |> int
+let convertToInt (a, b) = $"{a}{b}" |> int
 
 
 let exampleInput = readExample |> List.ofSeq
